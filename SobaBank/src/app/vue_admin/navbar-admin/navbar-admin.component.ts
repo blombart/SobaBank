ca0bea6b73478dd1a80baace493652c220f01580
blombart <benjamin.lombart@yahoo.fr>
2018-01-17 17:03:39 +0100

    mise en place de la nav bar pour la page admin

---
+++ SobaBank/src/app/vue_admin/navbar-admin/navbar-admin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-admin',
+  templateUrl: './navbar-admin.component.html',
+  styleUrls: ['../../bootstrap/css/bootstrap.css']
+})
+export class NavbarAdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
