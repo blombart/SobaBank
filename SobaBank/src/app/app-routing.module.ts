84f79d2261c347018ab42e72e27289ce20ba40f6
Sébastien Legay <33393241+Narutoseb@users.noreply.github.com>
2018-01-17 17:02:58 +0100

    redirection de la page d'accueil agent

--- SobaBank/src/app/app-routing.module.ts
+++ SobaBank/src/app/app-routing.module.ts
@@ -3,14 +3,17 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component'
 import {ForminscritionComponent} from './vue_public/forminscrition/forminscrition.component'
 import {ConnexionComponent } from './vue_public/connexion/connexion.component'
+import {AccueilAgentComponent} from './vue_agent/accueil-agent/accueil-agent.component'
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
-      
+     
+     {path: '', redirectTo: '/agent', pathMatch: 'full'},
      /*{path: '', component: AppComponent, pathMatch: 'full'},*/
      {path: 'inscription', component: ForminscritionComponent, pathMatch: 'full'},
-     {path: 'connexion', component: ConnexionComponent, pathMatch: 'full'}
+     {path: 'connexion', component: ConnexionComponent, pathMatch: 'full'},
+     {path: 'agent', component: AccueilAgentComponent, pathMatch: 'full'}
     ])
   ],
   exports: [RouterModule]
