fe94b348293c19d4f091f5ba28fe02c3ce6beb0c
blombart <benjamin.lombart@yahoo.fr>
2018-01-17 15:04:54 +0100

    fix problem

--- SobaBank/src/app/vue_public/navbar/navbar.component.ts
+++ SobaBank/src/app/vue_public/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['../bootstrap/css/bootstrap.css']
+  styleUrls: ['../../bootstrap/css/bootstrap.css']
 })
 export class NavbarComponent implements OnInit {
 
