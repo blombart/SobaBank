d867416b299dd98337ad007593deb0a020c46bc6
blombart <benjamin.lombart@yahoo.fr>
2018-01-17 14:30:04 +0100

    ajout vue_public

---
+++ SobaBank/src/app/vue_public/connexion/connexion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-connexion',
+  templateUrl: './connexion.component.html',
+  styleUrls: ['../bootstrap/css/bootstrap.css']
+})
+export class ConnexionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
