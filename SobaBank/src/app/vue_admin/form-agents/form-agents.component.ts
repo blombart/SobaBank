df236ced0ced9c1053152dbeaac84aa43440da91
blombart <benjamin.lombart@yahoo.fr>
2018-01-17 17:03:25 +0100

    mise en place du formulaire de creation d'agent

---
+++ SobaBank/src/app/vue_admin/form-agents/form-agents.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-agents',
+  templateUrl: './form-agents.component.html',
+  styleUrls: ['../../bootstrap/css/bootstrap.css']
+})
+export class FormAgentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
