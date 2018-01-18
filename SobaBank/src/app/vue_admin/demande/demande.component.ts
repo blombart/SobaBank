49fec4497e0a8ba4a374a2ef7e198b8ab84505cc
blombart <benjamin.lombart@yahoo.fr>
2018-01-18 11:31:12 +0100

    modification composant demande

--- SobaBank/src/app/vue_admin/demande/demande.component.ts
+++ SobaBank/src/app/vue_admin/demande/demande.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Demande } from '../test_class/demande';
 
 @Component({
   selector: 'app-demande',
   templateUrl: './demande.component.html',
-  styleUrls: ['../../bootstrap/css/bootstrap.css']
+  styleUrls: ['../../bootstrap/css/bootstrap.css',
+  '../../bootstrap/css/bootstrap-grid.css',
+  '../../bootstrap/css/bootstrap-reboot.css',
+  ]
 })
 export class DemandeComponent implements OnInit {
+	dem : Demande = new Demande("a","b","c", new Date());
+	dem1 : Demande = new Demande("a1","b1","c1", new Date());
+	dem2 : Demande = new Demande("a2","b2","c2", new Date());
+	dem3 : Demande = new Demande("a3","b3","c3", new Date());
+	demandes :  Demande[];
 
-  constructor() { }
+  constructor() {
+   }
 
   ngOnInit() {
+  	this.demandes= [this.dem, this.dem1, this.dem2, this.dem3];
   }
+
 
 }
