3e02562900367a1746eed4f04fb904308904a9a1
blombart <benjamin.lombart@yahoo.fr>
2018-01-18 11:30:55 +0100

    Ajout d'une classe test demande

---
+++ SobaBank/src/app/vue_admin/test_class/demande.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var Demande = /** @class */ (function () {
+    function Demande(id, agent, status) {
+        this.id = id;
+        this.agent = agent;
+        this.status = status;
+    }
+    return Demande;
+}());
+exports.Demande = Demande;
