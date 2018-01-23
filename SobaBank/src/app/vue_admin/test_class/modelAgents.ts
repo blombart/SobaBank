d3ef49238209665e9f504d421aa9023c0c7def99
blombart <benjamin.lombart@yahoo.fr>
2018-01-23 14:29:38 +0100

    modification du tableau d'agents

--- SobaBank/src/app/vue_admin/test_class/modelAgents.ts
+++ SobaBank/src/app/vue_admin/test_class/modelAgents.ts
@@ -1,42 +1,33 @@
-import {Agent} from './Agent';
+import {Agent} from './agent';
 
 export const agents: Agent[] = [
-{
-	id:1,
-	nom:"nom1",
-	prenom:"prenom1",
-	email:"email1@1.com",
-	mdp:"mdp1",
-	mat:"mat1",
-	clients:["1cli1","1cli2"],
-	demandes:["1dem1","1dem2"]
-},
-{	
-	id:2,
-	nom:"nom2",
-	prenom:"prenom2",
-	email:"email2@2.com",
-	mdp:"mdp2",
-	mat:"mat2",
-	clients:["2cli1","2cli2"],
-	demandes:["2dem1","2dem2"]
-},
-{	id:3,
-	nom:"nom3",
-	prenom:"prenom3",
-	email:"email3@3.com",
-	mdp:"mdp3",
-	mat:"mat3",
-	clients:["3cli1","3cli2"],
-	demandes:["3dem1","3dem2"]
-},
-{	id:4,
-	nom:"nom4",
-	prenom:"prenom4",
-	email:"email4@4.com",
-	mdp:"mdp4",
-	mat:"mat4",
-	clients:["4cli1","4cli2"],
-	demandes:["4dem1","4dem2"]
-}
+
+	new Agent(1,"nom1",
+	"prenom1",
+	"email1@1.com",
+	"mdp1",
+	"mat1",
+	["1cli1","1cli2"],
+	["1dem1","1dem2"]),
+new Agent(2,"nom2",
+	"prenom2",
+	"email2@2.com",
+	"mdp2",
+	"mat2",
+	["2cli1","2cli2"],
+	["2dem1","2dem2"]),
+new Agent(3,"nom3",
+	"prenom3",
+	"email3@3.com",
+	"mdp3",
+	"mat3",
+	["3cli1","3cli2"],
+	["3dem1","3dem2"]),
+new Agent(4,"nom4",
+	"prenom4",
+	"email4@4.com",
+	"mdp4",
+	"mat4",
+	["4cli1","4cli2"],
+	["4dem1","4dem2"]),
 ];
