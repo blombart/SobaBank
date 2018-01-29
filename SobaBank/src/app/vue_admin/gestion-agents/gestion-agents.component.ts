17cc864158f33c73bc51246cb0789c5a0dc42ab5
blombart <benjamin.lombart@yahoo.fr>
2018-01-29 19:12:21 +0100

    modification gestion des agents

--- SobaBank/src/app/vue_admin/gestion-agents/gestion-agents.component.ts
+++ SobaBank/src/app/vue_admin/gestion-agents/gestion-agents.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { agents } from '../../modeles/agent';
+import { Agent } from '../../modeles/agent';
+import { AgentService} from '../../Service/agent.service';
 
 @Component({
   selector: 'app-gestion-agents',
   templateUrl: './gestion-agents.component.html',
-  styleUrls: ['./gestion-agents.component.css']
+  styleUrls: ['../../bootstrap/css/bootstrap.css','../../bootstrap/css/bootstrap-grid.css']
 })
 export class GestionAgentsComponent implements OnInit {
 
-  constructor() { }
+	agents : Observable<Agent[]>;
+  constructor(private agentService: AgentService) { }
 
   ngOnInit() {
+  	this.agents = this.agentService.getAgents();
   }
 
-  setAgent(){
-  	
+  setAgent(){	
   }
 }
