4e6373a2cd79d3d45ba00f5db3f15907f4cae49b
blombart <benjamin.lombart@yahoo.fr>
2018-01-31 17:05:52 +0100

    Modification css

--- SobaBank/src/app/vue_admin/form-agents/form-agents.component.ts
+++ SobaBank/src/app/vue_admin/form-agents/form-agents.component.ts
@@ -10,7 +10,7 @@ import { Demande } from '../../modeles/demandes';
 @Component({
   selector: 'app-form-agents',
   templateUrl: './form-agents.component.html',
-  styleUrls: ['../../bootstrap/css/bootstrap.css']
+  styleUrls: ['../../bootstrap/css/bootstrap.css','./form-agents.component.css']
 })
 export class FormAgentsComponent implements OnInit {
 
