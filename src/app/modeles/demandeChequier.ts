6ca2090fc7b50fe1caa0d8c59245ca2fe395adfb
Narutoseb <sebastien.legay@laposte.net>
2018-02-08 16:51:39 +0100

    tableau chéquier

--- src/app/modeles/demandeChequier.ts
+++ src/app/modeles/demandeChequier.ts
@@ -7,7 +7,6 @@ import { User } from './user';
 export class DemandeChequier {
 	constructor(public id: number,
 		public dateDemande: Date,
-		public libelle: string,
 		public estAffecte: boolean,
 		public status: string,
 		public estValide: boolean,
@@ -22,7 +21,6 @@ export class DemandeChequier {
 export const demandeChequier: DemandeChequier[] = [
 
 	new DemandeChequier(1,new Date(),
-		"demande de chéquier",
 		false,
 		null,
 		false,
@@ -31,7 +29,6 @@ export const demandeChequier: DemandeChequier[] = [
 		),
 
 	new DemandeChequier(2,new Date(),
-    	"demande de chéquier",
 		false,
 		null,
 		false,
@@ -40,7 +37,6 @@ export const demandeChequier: DemandeChequier[] = [
 		),
 
 	new DemandeChequier(3,new Date(),
-		"demande de chéquier",
 		false,
 		null,
 		false,
@@ -49,7 +45,6 @@ export const demandeChequier: DemandeChequier[] = [
 		),
 
 	new DemandeChequier(4,new Date(),
-		"demande de chéquier",
 		false,
 		null,
 		false,
