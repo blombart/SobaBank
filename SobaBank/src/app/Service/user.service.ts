f4a90b8488338224e55b88cf76d62299a3ff61cb
habder <hadjaz.abder@yahoo.fr>
2018-02-13 09:43:43 +0100

    ajout Http a user.service

--- SobaBank/src/app/Service/user.service.ts
+++ SobaBank/src/app/Service/user.service.ts
@@ -4,13 +4,27 @@ import { Observable } from 'rxjs/Observable';
 import {User} from '../modeles/user';
 import {Users} from '../modeles/user';
 
+import { Http, Response } from "@angular/http";
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+
 
 @Injectable()
 export class UserService {
 
-  users = Users;
-  constructor() { }
+  private apiUrl = "http://localhost:8080/sobabank/admin/";
 
+  users = Users;
+  
+  constructor(private http: Http) { }
+
+
+  findAll(): Observable<User[]> {
+    return this.http.get(this.apiUrl + "agents")
+      .map((res: Response) => res.json())
+      .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+  }
 
   getUser(id) {
     return this.users[id-1];
