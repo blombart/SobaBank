0d65939d093a975aa12415ce7b9afd8fc6b65ae2
blombart <benjamin.lombart@yahoo.fr>
2018-02-13 16:30:05 +0100

    Ajout de la classe DemandeNouveauCompte

--- SobaBank/src/app/modeles/demandes.ts
+++ SobaBank/src/app/modeles/demandes.ts
@@ -1,5 +1,6 @@
 import {Agent} from './agent';
 import {Client} from './client';
+import {Compte} from './compte';
 
 export class Demande{
 	constructor(public id: number,
@@ -18,9 +19,7 @@ export class DemandeChequier extends Demande{
 		public libelle: string,
 		public estAffecte: boolean,
 		public status: string,
-		public estValide: boolean,
-		public agent: Agent,
-		public client: Client){
+		public estValide: boolean){
 		super(id,dateDemande,libelle,estAffecte,status,estValide)
 	}
 
@@ -32,22 +31,30 @@ export class DemandeModificationMdp extends Demande{
 		public libelle: string,
 		public estAffecte: boolean,
 		public status: string,
-		public estValide: boolean,
-		public agent: Agent,
-		public client: Client){
+		public estValide: boolean){
 		super(id,dateDemande,libelle,estAffecte,status,estValide)
 	}
-
 }
+
+export class DemandeNouveauCompte extends Demande{
+	constructor(public id: number,
+		public dateDemande: Date,
+		public libelle: string,
+		public estAffecte: boolean,
+		public status: string,
+		public estValide: boolean,
+		public newCompte : Compte){
+		super(id,dateDemande,libelle,estAffecte,status,estValide)
+	}
+}
+
 
 	export const demandeModificationMdp: DemandeModificationMdp[] = [
 	new DemandeModificationMdp(1,new Date(),
 		"demande chequier",
 		false,
 		null,
-		false,
-		null,
-		null
+		false
 		)
 
 ]
@@ -57,32 +64,24 @@ export const demandeChequier: DemandeChequier[] = [
 	new DemandeChequier(1,new Date(),"demande chequier",
 		false,
 		null,
-		false,
-		null,
-		null
+		false
 		),
 
 	new DemandeChequier(2,new Date(),"demande chequier",
 		false,
 		null,
-		false,
-		null,
-		null
+		false
 		),
 
 	new DemandeChequier(3,new Date(),"demande chequier",
 		false,
 		null,
-		false,
-		null,
-		null
+		false
 		),
 
 	new DemandeChequier(4,new Date(),"demande chequier",
 		false,
 		null,
-		false,
-		null,
-		null
+		false
 		)
 ]
