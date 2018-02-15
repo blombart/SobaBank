b6e1459a8f266f68aa838a64fd492e00542be866
blombart <benjamin.lombart@yahoo.fr>
2018-02-15 14:55:36 +0100

    mise a niveau branchbenjamin

--- sobabank_back/src/com/bl/service/IAgentService.java
+++ sobabank_back/src/com/bl/service/IAgentService.java
@@ -1,22 +1,33 @@
 package com.bl.service;
 
+import java.io.File;
 import java.util.List;
 
 import com.bl.model.Client;
-import com.bl.model.Demande;
+import com.bl.model.DemandeChequier;
+import com.bl.model.DemandeModifMdp;
+import com.bl.model.DemandeNouveauCompte;
 import com.bl.model.DemandeOuvertureCompte;
 
 public interface IAgentService {
 
 	public List<Client> getAllClient(Long id);
 	
-	public List<Demande> getAllDemandeByType(String type);
-	
-	public List<DemandeOuvertureCompte> getAllDemandeOuvertureCompte();
-	
 	public Client updateClient(Client client);
 	
-	public DemandeOuvertureCompte updateDemandeOuvertureCompte(DemandeOuvertureCompte dem);
+	public List<File> getDocumentsForClient(Long idClient);
 	
-	public Demande updateDemande(Demande dem);
-}
+	public List<DemandeChequier> getAllDemandeChequier(Long idAgent);
+	
+	public List<DemandeModifMdp> getAllDemandeMdp(Long idAgent);
+	
+	public List<DemandeNouveauCompte> getAllDemandeNouveauCompte(Long idAgent);
+	
+	public DemandeChequier updateDemandeChequier(DemandeChequier dem);
+	
+	public DemandeModifMdp updateDemandeModifMdp(DemandeModifMdp dem);
+	
+	public DemandeNouveauCompte updateDemandeNouveauCompte(DemandeNouveauCompte dem);
+	
+	public List<DemandeOuvertureCompte> getAllDemandeOuvertureCompte(Long idAgent);
+ }
