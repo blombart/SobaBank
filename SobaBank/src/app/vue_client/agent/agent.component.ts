e882abe77e2d57d4e7a5b4c9130bbda00fadd45f
blombart <benjamin.lombart@yahoo.fr>
2018-02-20 22:29:26 +0100

    Creation d'une demande de nouveau compte pour un client

--- SobaBank/src/app/vue_client/agent/agent.component.ts
+++ SobaBank/src/app/vue_client/agent/agent.component.ts
@@ -2,6 +2,8 @@
 import { Agent} from '../../modeles/agent';
 import { ClientService} from '../../Service/client.service';
 import { Observable } from 'rxjs/Observable';
+import { FormBuilder,Validators, FormGroup, FormControl} from '@angular/forms';
+import { CookieService} from 'angular2-cookie/core';
 
 @Component({
   selector: 'app-agent',
@@ -11,19 +13,27 @@ import { Observable } from 'rxjs/Observable';
 })
 export class AgentComponent implements OnInit {
 
+  compteForm: FormGroup;
+  id:number;
 
-	ag: Agent;
-	
-
-
-  constructor(private clientService : ClientService){}
+  constructor(private clientService : ClientService,private _fb: FormBuilder,, private cookieService: CookieService){}
 
   ngOnInit() {
-  	this.clientService.getAgent(1).subscribe(
-  		agent => {this.ag = agent;});
+     this.id = Number(this.cookieService.get("id"));
+    this.createForm();
 }
 
+createForm(){
+    this.compteForm = this._fb.group({
+      solde: ['', [Validators.required]],
+      compteSelected: ['']
+    });
+  }
 
+onSubmit(){
+  //TODO modifier la demande de nouveau compte et envoyer un type de compte et le solde en param plutot qu'un objet compte 
+  this.clientService.createDemandeNouveauCompte(this.id,);
+}
 
 
 }
