50a4b31736871fe98c4034685b61dbfdb5e033f8
osta21 <35526706+osta21@users.noreply.github.com>
2018-02-21 14:21:13 +0100

    Update DemandeDAOImpl.java

--- MonProjet/src/main/java/com/bl/dao/impl/DemandeDAOImpl.java
+++ MonProjet/src/main/java/com/bl/dao/impl/DemandeDAOImpl.java
@@ -9,7 +9,10 @@ import org.springframework.stereotype.Repository;
 
 import com.bl.dao.AbstractDao;
 import com.bl.dao.IDemandeDAO;
+import com.bl.model.Agent;
+import com.bl.model.Client;
 import com.bl.model.Demande;
+import com.bl.model.DemandeChequier;
 
 @Repository("demandeDao")
 public class DemandeDAOImpl extends AbstractDao<Long, Demande> implements IDemandeDAO{
@@ -74,6 +77,32 @@ public class DemandeDAOImpl extends AbstractDao<Long, Demande> implements IDeman
 	public Demande getDemandeById(Long id) {
 		Demande demande =getByKey(id);
 		return demande;
+	}
+
+	@Override
+	public List<DemandeChequier> getAllDemandesChequierByIdAgent(Long idAgent) {
+		
+//		List<DemandeChequier> demandes = getEntityManager().createNativeQuery("select demande.type_demande, demande.id, demande.dateDemande, demande.libelle, demande.status, demande.newCompte_id from Demande "
+//				+ "join user_demande on demande.id = user_demande.demandes_id "
+//				+ "join user on user.id = user_demande.user_id "
+//				+ "join user_user on user.id = user_user.agents_id "
+//				+ "where user_user.agents_id = ?1").setParameter("1", idAgent).getResultList();
+		
+//		Agent agent = (Agent) getEntityManager().createQuery("select c from DemandeChequier c ").getSingleResult();
+//		
+//		ArrayList<Client> clients = new ArrayList<Client>();
+		
+		@SuppressWarnings("unchecked")
+		List<Object> results = getEntityManager().createNativeQuery("select dem.id from demande dem where dem.id in "
+				+ "( select demandes_id from user_demande where user_id in (select clients_id from user_user where user_id =? )) "
+				+ "and type_demande= 'chequier' ").setParameter(1, idAgent).getResultList();
+		List<DemandeChequier> chequiers = new ArrayList<DemandeChequier>();
+		for(Object o : results){
+			DemandeChequier cheque = (DemandeChequier)getByKey(Long.parseLong(o.toString()));
+			chequiers.add(cheque);
+ 		}
+		
+		return chequiers;
 	}	
 	
 
